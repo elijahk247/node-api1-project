197b09df30fcacfd5fd2e82848b1a92bc0cd5678
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-10-01 18:51:41 -0400

    Added GET request for all users and post request; need to work on get by id, put by id, and remove by id requests

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const server = express();
 
 // GET request for landing page/initial endpoint of api
 server.get('/', (req, res) => {
-  res.status(200).json({ intiailMessage: 'welcome to the API' });
+  res.status(200).json({ intialMessage: 'welcome to the API' });
 })
 
 const users = [
@@ -21,12 +21,27 @@ const users = [
   }
 ]
 
-// GET request that returns an array of users 
+// GET request that returns an array of users, throws an error if nothing found  
 server.get('/api/users', (req, res) => {
-
+  if(users) {
+    res.status(200).json({ resource: users });
+  } else {
+    res.status(500).json({ errorMessage: 'The users information could not be retrieved' });
+  }
 })
 
 // POST requests
 server.post('/api/users', (req, res) => {
+  const data = req.body;
 
+  if(data.name === undefined || data.bio === undefined) {
+    res.status(400).json({ errorMessage: 'Please provide name and bio' })
+  } else if(users) {
+    users.push({ id: shortid.generate(), ...data });
+    res.status(201).json({ resource: users });
+  } else {
+    res.status(500).json({ errorMessage: 'There was an error while saving user to the database' });
+  }
 })
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require('./api/sever.js');
+const server = require('./api/server.js');
 
 const port = 5000;
 server.listen(port, () => console.log(' server is running...'));
